8d4681781cea0db907021cc9c148b77c31eedc50
Brandon Smith <freedom@reardencode.com>
2011-05-05 11:14:45 -0700

    MD5: Compute rather than store constants
    Turns out javascript's math functions can quickly make most of the MD5
    constants rather than having the table in there.

--- core/md5.js
+++ core/md5.js
@@ -12,6 +12,7 @@
  * @class MD5, 128 bits.
  */
 sjcl.hash.md5 = function (hash) {
+  if (!this._T[0]) { this._precompute(); }
   if (hash) {
     this._h = hash._h.slice(0);
     this._buffer = hash._buffer.slice(0);
@@ -127,32 +128,37 @@ sjcl.hash.md5.prototype = {
   
   _S:[[7, 12, 17, 22], [5, 9, 14, 20], [4, 11, 16, 23], [6, 10, 15, 21]],
 
-  _T:[
-    [ 0, 0xd76aa478], [ 1, 0xe8c7b756], [ 2, 0x242070db], [ 3, 0xc1bdceee],
-    [ 4, 0xf57c0faf], [ 5, 0x4787c62a], [ 6, 0xa8304613], [ 7, 0xfd469501],
-    [ 8, 0x698098d8], [ 9, 0x8b44f7af], [10, 0xffff5bb1], [11, 0x895cd7be],
-    [12, 0x6b901122], [13, 0xfd987193], [14, 0xa679438e], [15, 0x49b40821],
-    [ 1, 0xf61e2562], [ 6, 0xc040b340], [11, 0x265e5a51], [ 0, 0xe9b6c7aa],
-    [ 5, 0xd62f105d], [10, 0x02441453], [15, 0xd8a1e681], [ 4, 0xe7d3fbc8],
-    [ 9, 0x21e1cde6], [14, 0xc33707d6], [ 3, 0xf4d50d87], [ 8, 0x455a14ed],
-    [13, 0xa9e3e905], [ 2, 0xfcefa3f8], [ 7, 0x676f02d9], [12, 0x8d2a4c8a],
-    [ 5, 0xfffa3942], [ 8, 0x8771f681], [11, 0x6d9d6122], [14, 0xfde5380c],
-    [ 1, 0xa4beea44], [ 4, 0x4bdecfa9], [ 7, 0xf6bb4b60], [10, 0xbebfbc70],
-    [13, 0x289b7ec6], [ 0, 0xeaa127fa], [ 3, 0xd4ef3085], [ 6, 0x04881d05],
-    [ 9, 0xd9d4d039], [12, 0xe6db99e5], [15, 0x1fa27cf8], [ 2, 0xc4ac5665],
-    [ 0, 0xf4292244], [ 7, 0x432aff97], [14, 0xab9423a7], [ 5, 0xfc93a039],
-    [12, 0x655b59c3], [ 3, 0x8f0ccc92], [10, 0xffeff47d], [ 1, 0x85845dd1],
-    [ 8, 0x6fa87e4f], [15, 0xfe2ce6e0], [ 6, 0xa3014314], [13, 0x4e0811a1],
-    [ 4, 0xf7537e82], [11, 0xbd3af235], [ 2, 0x2ad7d2bb], [ 9, 0xeb86d391]
-  ],
+  _X:[0,  1,  2,  3, 4,  5,  6,  7, 8,  9, 10, 11, 12, 13, 14, 15,
+      1,  6, 11,  0, 5, 10, 15,  4, 9, 14,  3,  8, 13,  2,  7, 12,
+      5,  8, 11, 14, 1,  4,  7, 10, 13,  0,  3,  6, 9, 12, 15,  2,
+      0,  7, 14,  5, 12,  3, 10,  1, 8, 15,  6, 13, 4, 11,  2,  9],
+
+  /* Will be precomputed */
+  _T:[],
+  /*
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+  */
+  _precompute:function() {
+    var i;
+    for (i=0; i<64; i++) {
+      this._T[i] = ((0xffffffff+1) * Math.abs(Math.sin(i+1)))|0;
+    }
+  },
 
   /**
    * The MD5 outer function
    * @private
    */
   _FFGGHHII:function(a, b, c, d, w, f, s, i, j) {
-    var s = s[j], tx = this._T[i+j];
-    a += f(b, c, d) + w[tx[0]] + tx[1];
+    var s = s[j];
+    a += f(b, c, d) + w[this._X[i+j]] + this._T[i+j];
     return ((a << s) | (a >>> 32-s)) + b;
   },
 
